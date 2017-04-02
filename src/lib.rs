8301dd9653f3bb07c36b14e2442d3ebd37d958f2
Maurits van Altvorst <mvanaltvorst@icloud.com>
2017-04-02 09:28:48 +0200

    Implemented Bitmap parsing and Matrix exporting

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 extern crate ws;
+extern crate image;
 
 use std::io::BufReader;
 use std::io::Read;
 use std::fs::File;
+use std::path::Path;
 
 #[cfg(test)]
 mod tests {
@@ -34,17 +36,55 @@ impl Bitmap {
     }
     
     pub fn to_matrix(&self) -> Matrix {
-        let iter = self.raw_data.iter();
+        let mut iter = self.raw_data.iter();
         let mut matrix = [[0u8; 1000]; 1000];
         for y in 0..1000 {
             for x in 0..500 {
-                let datum = iter.next();
-                println!("{:?}", datum);
+                let datum = match iter.next() {
+                    Some(b) => *b,
+                    None => panic!("read too many bytes"),
+                };
+                // println!("{:?}", datum);
+                let color1 = datum >> 4;
+                let color2 = datum & 0b1111; // Last 4 bits
+
+                matrix[x*2][y] = color1;
+                matrix[x*2 + 1][y] = color2;
 
             }
         }
         Matrix{
             matrix: matrix
         }
+    }
+}
+
+impl Matrix {
+    pub fn to_image(&self, filename: &str) {
+        let mut imgbuf = image::ImageBuffer::new(1000, 1000);
+        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+            match self.matrix[x as usize][y as usize]{
+                0  => *pixel = image::Rgb([255, 255, 255]),
+                1  => *pixel = image::Rgb([228, 228, 228]),
+                2  => *pixel = image::Rgb([136, 136, 136]),
+                3  => *pixel = image::Rgb([34,  34,  34 ]),
+                4  => *pixel = image::Rgb([255, 167, 209]),
+                5  => *pixel = image::Rgb([229, 0  , 0  ]),
+                6  => *pixel = image::Rgb([229, 149, 0  ]),
+                7  => *pixel = image::Rgb([160, 106, 66 ]),
+                8  => *pixel = image::Rgb([229, 217, 0  ]),
+                9  => *pixel = image::Rgb([148, 224, 68 ]),
+                10 => *pixel = image::Rgb([2  , 190, 1  ]),
+                11 => *pixel = image::Rgb([0  , 211, 221]),
+                12 => *pixel = image::Rgb([0  , 131, 199]),
+                13 => *pixel = image::Rgb([0  , 0  , 234]),
+                14 => *pixel = image::Rgb([207, 110, 228]),
+                15 => *pixel = image::Rgb([130, 0  , 128]),
+                _  => unreachable!()
+            }
+        }
+
+        let ref mut fout = File::create(&Path::new(filename)).unwrap();
+        let _ = image::ImageRgb8(imgbuf).save(fout, image::PNG);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate redditPlaceHeatmap;
 
 use redditPlaceHeatmap::{Bitmap, Matrix};
+use std::env::args;
 
 fn main() {
-    let bm = Bitmap::from_file("export.csv").unwrap();
+    let mut bm = Bitmap::from_file(&args().nth(1).expect("did not get input csv argument")).unwrap();
     // println!("{:?}", bm);
     let matrix = bm.to_matrix();
+    matrix.to_image("out.png")
 }
